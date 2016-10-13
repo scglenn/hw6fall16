44ff9cf7fb56aaa2cf154cbc857d283ecdcf22d2
Sam Glenn <samglennss3@gmail.com>
2016-10-13 23:55:38 +0000

    small changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,14 +3,15 @@ class Movie < ActiveRecord::Base
     %w(G PG PG-13 NC-17 R)
   end
   
-# class Movie::InvalidKeyError < StandardError ; end
+class Movie::InvalidKeyError < StandardError ; end
   
-#  def self.find_in_tmdb(string)
-#    begin
-#      Tmdb::Movie.find(string)
-#    rescue Tmdb::InvalidApiKeyError
-#        raise Movie::InvalidKeyError, 'Invalid API key'
-#    end
-#  end
+  def self.find_in_tmdb(string)
+    begin
+      Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
+      Tmdb::Movie.find(string)
+    rescue Tmdb::InvalidApiKeyError
+        raise Movie::InvalidKeyError, 'Invalid API key'
+    end
+  end
 
 end
